25580ae8140f1d92da4019290cfc376c957d91bf
Goiko <ocelote@gmail.com>
2014-09-16 18:49:52 +0200

    :tada: Added .gitattributes & .gitignore files

---
+++ java 01_introduccion/src/MiClase.java
@@ -0,0 +1,10 @@
+public class MiClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// empezamos con las operaciones de entrada y salida por pantalla. La funcion muestra texto por pantalla
+			System.out.print("Hello Kitty!!");
+	}
+
+}
+ 
