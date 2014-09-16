97b191ec4f4281ba5f9b110c84628e4628da230a
Goiko <ocelote@gmail.com>
2014-09-16 19:57:58 +0200

    añadir intro de java

--- java 01_introduccion/src/MiClase.java
+++ java 01_introduccion/src/MiClase.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// empezamos con las operaciones de entrada y salida por pantalla. La funcion muestra texto por pantalla
-			System.out.print("Hello Kitty!!");
+		System.out.print("Hello Kitty!!");
+			
+		// funcion y tipos de variables. 
+		// variables en java :integer, string, boolean 
+		// integer = números enteros
+		// string = cadenas de caracteres
+		// boolean = 0/1
+		// println() <- imprime y realiza salto de línea
+		System.out.println("Introduce tu nombre");
+		// declaramos la variable entrada que pertenece a la clase scanner y leerá del teclado
+		Scanner entrada = new Scanner(System.in);
+		// creo una variable para guardar el nombre del usuario
+		String nombre = entrada.nextLine();
+		System.out.print("hola " +nombre+" qué tal");
+		
 	}
-
-}
- 
+} 
