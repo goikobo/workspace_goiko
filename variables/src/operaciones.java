ce6ebf4cf52aa09538b4620f641f12f969022198
Goiko <ocelote@gmail.com>
2014-09-16 19:56:55 +0200

    añadir variables

---
+++ variables/src/operaciones.java
@@ -0,0 +1,13 @@
+
+public class operaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=5;
+		int num2=6;
+		int resultado = num1*num2;
+	System.out.println(resultado);
+
+	}
+
+}
