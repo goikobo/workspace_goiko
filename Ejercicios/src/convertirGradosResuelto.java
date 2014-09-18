7fbbab85cdd576d83c258012a2501c189c26cb27
uno <uno@dante>
2014-09-18 22:19:24 +0200

    proyecto sólo con los ejercicios. A partir del 2º día.

---
+++ Ejercicios/src/convertirGradosResuelto.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class convertirGradosResuelto {
+
+	public static void main(String[] args) {
+		float temp;
+		Scanner entrada = new Scanner(System.in);
+		String tipo;
+		float resultado;
+		//controla que el numero introducido este entre 0 y 100
+		do{
+			System.out.println("Introduce temperatura");
+			temp = entrada.nextFloat();
+		}while (temp<0 || temp > 100);
+		//controla que se elija el tipo de conversion: f o c		
+		do{
+			System.out.println("Introduce la conversión (f/c)");
+			tipo = entrada.next();
+		}while (!(tipo.equals("f") || tipo.equals("c")));
+		//realiza las operaciones
+		if (tipo.equals("f")){
+			resultado = 32+(9*temp/5);
+		}else{
+			resultado = (temp-32)*((float) 1.8);
+		}
+		
+		System.out.println(resultado);
+		entrada.close();
+		
+	}
+
+}
