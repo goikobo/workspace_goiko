903589cbc0d58bf793a368ce1d692486020bd0e8
alumno214 <alumno214@Java-22.marketing.local>
2014-09-30 20:22:17 +0200

    funciones

--- funciones/src/funcMatrizMaxMinMed.java
+++ funciones/src/funcMatrizMaxMinMed.java
@@ -1,7 +1,39 @@
 
 public class funcMatrizMaxMinMed {
+	
+	public static int maximo(int[]vector){
+		//funcion que busca el valor máximo en un array unidimensional
+		int result = Integer.MIN_VALUE;
+		for (int i=0;i<vector.length;i++){
+			if (vector[i]>result){
+				result = vector[i];
+			}
+		}
+		return result;
+	}
+	
+	public static int minimo(int[]vector){
+		//funcion que busca el valor máximo en un array unidimensional
+		int result = Integer.MAX_VALUE;
+		for (int i=0;i<vector.length;i++){
+			if (vector[i]<result){
+				result = vector[i];
+			}
+		}
+		return result;
+	}
+	
+	public static int media(int[]vector){
+		int medMed = 0;
+		for (int i= 0;i>vector.length;i++){
+			medMed = medMed + vector[i];
+		}
+		return (medMed/vector.length);
+	}
 
 	public static void main(String[] args) {
+		
+
 		
 		int[][] matriz = new int[10][10];
 		
